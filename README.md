30199cb01e3a1a4ecc59f1769f4c825168751dd6
braviss <doctype.developer@gmail.com>
2024-02-25 16:15:00 +0200

    [add] model article and category

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Django Blog
 ___
 ![https://img.shields.io/badge/Python-3.10.6-blue](https://img.shields.io/badge/Python-3.10.6-blue)
+![https://pillow.readthedocs.io/en/latest/index.html](https://img.shields.io/badge/Pillow-10.2.0-blue)
+![https://django-tinymce.readthedocs.io/en/latest/installation.html](https://img.shields.io/badge/Tinymce-3.7.1-blue)
---
+++ article/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from article.models import Article, Category
+from django.utils import timezone
+
+
+@admin.action(description="Mark selected stories as published")
+def make_published(modeladmin, request, queryset):
+    queryset.update(status="pu")
+
+
+@admin.action(description="Raise article")
+def raise_article(modeladmin, request, queryset):
+    queryset.update(created_at=timezone.now())
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "created_at", "image", "slug",]
+    list_filter = ('status', 'created_at')
+    ordering = ["-created_at"]
+    exclude = ["last_modify_time"]
+    actions = [make_published, raise_article]
+    prepopulated_fields = {'slug': ('title',)}
+    fields = ('title',
+              'slug',
+              'body',
+              'category',
+              'image',
+              'status',
+              'author',
+              'created_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug",]
+    prepopulated_fields = {'slug': ('name',)}
